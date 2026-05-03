32619561dab704b87b3a8de3b875c5bd734a0fa5
Prasanna Poovelu <prasanna.poovelu23@gmail.com>
2026-05-03 08:07:40 -0700

    final change

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio-website" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio-website/" : "",
 };
 
 module.exports = nextConfig;
